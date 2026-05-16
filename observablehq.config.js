7ed6b8c013438b39a39664c173955cc38e307b6a
juandnn <diego.niebles@hotmail.com>
2026-05-16 17:05:56 -0500

    notebook y agrupaciones

--- observablehq.config.js
+++ observablehq.config.js
@@ -23,11 +23,11 @@ export default {
   root: "src",
 
   // Some additional configuration options and their defaults:
-  // theme: "default", // try "light", "dark", "slate", etc.
+  theme: "dark", // try "light", "dark", "slate", etc.
   // header: "", // what to show in the header (HTML)
   // footer: "Built with Observable.", // what to show in the footer (HTML)
   // sidebar: true, // whether to show the sidebar
-  // toc: true, // whether to show the table of contents
+  toc: false, // whether to show the table of contents
   // pager: true, // whether to show previous & next links in the footer
   // output: "dist", // path to the output root for build
   // search: true, // activate search
